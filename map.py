c8309f38f8c45a43a6cec874334bbedc74efbea2
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2021-09-24 17:30:33 +0300

    Small fix to map generation

--- map.py
+++ map.py
@@ -19,10 +19,10 @@ class Map:
         self.walls[::, 0] = 1
         self.walls[::, -1] = 1
 
-    def _random_split(self, x, y, min_room_size=3):
+    def _random_split(self, x, y, cross_doors=False, cross_doors_rand=False):
         """Adds vertical and horizontal walls until reaches min_room_size"""
 
-        if abs(x[0] - x[1]) <= min_room_size or abs(y[0] - y[1]) <= min_room_size:
+        if abs(x[0] - x[1]) <= 3 or abs(y[0] - y[1]) <= 3:
             return
         else:
             # random walls (+2, -2) so that the walls are 1 block wide,
@@ -35,15 +35,22 @@ class Map:
             self.walls[x[0] + 2: x[1] - 1, y_wall_pos] = 1
 
             # creating passages in the middle of cross-wall
-            self.walls[x_wall_pos + 1, y_wall_pos] = 0
-            #self.walls[x_wall_pos, y_wall_pos] = 0
-            self.walls[x_wall_pos - 1, y_wall_pos] = 0
+            if cross_doors:
+                if not cross_doors_rand:
+                    self.walls[x_wall_pos + 1, y_wall_pos] = 0
+                    self.walls[x_wall_pos - 1, y_wall_pos] = 0
+                    self.walls[x_wall_pos, y_wall_pos + 1] = 0
+                    self.walls[x_wall_pos, y_wall_pos - 1] = 0
 
-            self.walls[x_wall_pos, y_wall_pos + 1] = 0
-            #self.walls[x_wall_pos, y_wall_pos] = 0
-            self.walls[x_wall_pos, y_wall_pos - 1] = 0
+                elif np.random.randn() > 0:
+                    self.walls[x_wall_pos + 1, y_wall_pos] = 0
+                    self.walls[x_wall_pos - 1, y_wall_pos] = 0
 
-            #recursive call
+                else:
+                    self.walls[x_wall_pos, y_wall_pos + 1] = 0
+                    self.walls[x_wall_pos, y_wall_pos - 1] = 0
+
+            # recursive call
             self._random_split(x=(x[0], x_wall_pos), y=(y[0], y_wall_pos))
             self._random_split(x=(x_wall_pos, x[1]), y=(y[0], y_wall_pos))
             self._random_split(x=(x[0], x_wall_pos), y=(y_wall_pos, y[1]))
